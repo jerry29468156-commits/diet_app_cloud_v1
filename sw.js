5612d33d69b8777ae523e9db7f022328ef9d0311
jerry29468156-commits <jerry29468156@gmail.com>
2026-06-24 17:04:12 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME='diet-app-v2-2-cache';
-const ASSETS=['./','./index.html','./app_config.js','./manifest.webmanifest'];
+const CACHE_NAME='diet-app-v2-2-1-cache';
+const ASSETS=['./','./index.html','./app_config.js','./manifest.webmanifest','./favicon.svg','./icon-192.png','./icon-512.png','./apple-touch-icon.png'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(ASSETS)).catch(()=>{}));self.skipWaiting();});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))));self.clients.claim();});
 self.addEventListener('fetch',event=>{if(event.request.method!=='GET')return;event.respondWith(fetch(event.request).then(res=>{const copy=res.clone();caches.open(CACHE_NAME).then(cache=>cache.put(event.request,copy)).catch(()=>{});return res;}).catch(()=>caches.match(event.request).then(cached=>cached||caches.match('./index.html'))));});
